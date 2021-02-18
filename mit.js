e621bd69464cb648e1c444a1a0e4b7354fc3ed8b
Suzie K <suziekim.dev@gmail.com>
2021-02-18 17:49:49 -0500

    clears old code mit.js

--- mit.js
+++ mit.js
@@ -1,10 +1,7 @@
 var mit = {
     city: 'Cambridge',
-    // colors: ['pink', 'red'],
     colors: 'Silver Gray',
     mascot: "robot",
-    // founded: 1900,
     founded: 1875,
-    // motto: 'Carpe diem',
     motto: 'Mens',
 };
